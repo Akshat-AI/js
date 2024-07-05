e6ea913ae7a0e544197a7b295cf98119707bf642
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-07-05 18:23:16 +0530

    added Template_String.js

---
+++ Template_String.js
@@ -0,0 +1,6 @@
+var user = "Akshat"
+var greet = `Hello ${user} Welcome to program`
+console.log(greet)
+// document.write vs console.log 
+// document.write me the formatting is not visble
+//while console.log me formatting is visible
